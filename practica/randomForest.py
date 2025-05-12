509d54a56a8c8974799bb7d6f16264df44675000
CyanideData2 <danielhurtadolabandeira@gmail.com>
2025-05-12 13:57:22 +0200

    pull split into class

--- practica/randomForest.py
+++ practica/randomForest.py
@@ -8,8 +8,10 @@ from typing import Generic
 from numpy.typing import NDArray
 from decisionTree import Leaf, Parent, Node
 from dataset import Dataset
+from splitting import Split
 from measure import Impurity
 import logging
+
 
 
 class Forest:
@@ -20,7 +22,7 @@ class Forest:
         max_depth: int,
         ratio_samples: float,
         num_random_features: int,
-        criterion: Impurity,
+        split: Split,
         parallel: bool,
     ) -> None:
         self.num_trees: int = num_trees
@@ -28,7 +30,7 @@ class Forest:
         self.max_depth: int = max_depth
         self.ratio_samples: float = ratio_samples
         self.num_random_features: int = num_random_features
-        self.criterion: Impurity = criterion
+        self.split: Split = split
         self.parallel: bool = parallel
         self.decision_trees: list[Node] = []
         self.time = 0
@@ -132,7 +134,7 @@ class Forest:
             best_threshold,
             minimum_cost,
             best_split,
-        ) = self._best_split(idx_features, dataset)
+        ) = self.split._best_split(idx_features, dataset)
 
         left_dataset, right_dataset = best_split
         assert left_dataset.num_samples > 0 or right_dataset.num_samples > 0
@@ -147,91 +149,3 @@ class Forest:
             node.right_child = self._make_node(right_dataset, depth + 1)
         return node
 
-    @abstractmethod
-    def _best_split(
-        self, idx_features: NDArray[np.int64], dataset: Dataset
-    ) -> tuple[
-        np.int64, np.float64, np.float64, list[Dataset]
-    ]:   # find the best pair (feature, threshold) by exploring all possible pairs
-        pass
-
-    def _CART_cost(
-        self, left_dataset: Dataset, right_dataset: Dataset
-    ) -> np.float64:
-        # the J(k,v) equation in the slides, using Gini
-        left_len = np.float64(left_dataset.num_samples)
-        right_len = np.float64(right_dataset.num_samples)
-        critereon: Impurity = self.criterion
-
-        return left_len / (left_len + right_len) * critereon.purity(
-            left_dataset
-        ) + right_len / (left_len + right_len) * critereon.purity(
-            right_dataset
-        )
-
-
-class RandomForest(Forest):
-    def _best_split(
-        self, idx_features: NDArray[np.int64], dataset: Dataset
-    ) -> tuple[
-        np.int64, np.float64, np.float64, list[Dataset]
-    ]:   # find the best pair (feature, threshold) by exploring all possible pairs
-        int_max = np.int64(np.iinfo(np.int64).max)
-        best_feature_index, best_threshold, minimum_cost = (
-            int_max,
-            np.float64(np.inf),
-            np.float64(np.inf),
-        )
-        best_split: list[Dataset] | None = None
-        idx: np.int64
-        for idx in idx_features:
-            values = np.unique(dataset.X[:, idx])
-            val: np.float64
-            for val in values:
-                left_dataset, right_dataset = dataset.split(idx, val)
-                cost = self._CART_cost(left_dataset, right_dataset)   # J(k,v)
-                if cost < minimum_cost:
-                    (
-                        best_feature_index,
-                        best_threshold,
-                        minimum_cost,
-                        best_split,
-                    ) = (idx, val, cost, [left_dataset, right_dataset])
-
-        if best_split == None or best_feature_index == np.inf:
-            raise Exception('Dataset is empty')
-
-        return best_feature_index, best_threshold, minimum_cost, best_split
-
-
-class ExtraTrees(Forest):
-    def _best_split(
-        self, idx_features: NDArray[np.int64], dataset: Dataset
-    ) -> tuple[
-        np.int64, np.float64, np.float64, list[Dataset]
-    ]:   # find the best pair (feature, threshold) by exploring all possible pairs
-        int_max = np.int64(np.iinfo(np.int64).max)
-        best_feature_index, best_threshold, minimum_cost = (
-            int_max,
-            np.float64(np.inf),
-            np.float64(np.inf),
-        )
-        best_split: list[Dataset] | None = None
-        idx: np.int64
-        for idx in idx_features:
-            values = np.unique(dataset.X[:, idx])
-            val: np.float64 = np.random.choice(values)
-            left_dataset, right_dataset = dataset.split(idx, val)
-            cost = self._CART_cost(left_dataset, right_dataset)   # J(k,v)
-            if cost < minimum_cost:
-                (
-                    best_feature_index,
-                    best_threshold,
-                    minimum_cost,
-                    best_split,
-                ) = (idx, val, cost, [left_dataset, right_dataset])
-
-        if best_split == None or best_feature_index == np.inf:
-            raise Exception('Dataset is empty')
-
-        return best_feature_index, best_threshold, minimum_cost, best_split
